d74d1d65f71b456d1a01fe6df800a6f7b16b8dad
Max Beatty <max@isocket.com>
2012-12-07 15:28:26 -0800

    use express to serve html

--- login/login.js
+++ login/login.js
@@ -1,1 +1,17 @@
-// node
+// node solution
+
+// using express because I'm lazy
+var express = require('express');
+var app = express();
+
+// getting super lazy
+var fs = require('fs');
+
+app.get('/', function(req, res){
+  fs.readFile(__dirname + '/login.html', 'utf8', function(err, text) {
+    res.send(text);
+  });
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
