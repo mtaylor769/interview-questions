3d115fb9edde1ac3cbfb9524fafe40e29f0494cc
Max Beatty <max@isocket.com>
2012-12-07 16:23:23 -0800

    php solved (minimally)

--- login/login.php
+++ login/login.php
@@ -1,1 +1,24 @@
 <?php
+
+$USERNAME = 'isocket';
+$PASSWORD = 'rocks!';
+$data = array();
+
+if ($_POST && isset($_POST['username']) && isset($_POST['passwd'])) {
+  if ($_POST['username'] == $USERNAME && $_POST['passwd'] == $PASSWORD) {
+    $data['success'] = TRUE;
+  } else {
+
+    $data['errors'] = array();
+
+    if ($_POST['username'] != $USERNAME) {
+      array_push($data['errors'], 'username');
+    }
+
+    if ($_POST['passwd'] != $PASSWORD) {
+      array_push($data['errors'], 'passwd');
+    }
+  }
+
+  echo json_encode($data);
+}
