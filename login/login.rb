d26da124ecf6da28c10103ab79d269d918fd5eec
Chris Baclig <chris.baclig@gmail.com>
2012-12-07 18:02:35 -0800

    Ruby solution with all responses returned by the root path

--- login/login.rb
+++ login/login.rb
@@ -1,1 +1,24 @@
 require 'sinatra'
+require 'json'
+
+USERNAME = 'isocket';
+PASSWORD = 'rocks!';
+
+get '/' do
+  send_file File.dirname(__FILE__) + '/login.html'
+end
+
+post '/' do
+  result = {}
+
+  if params[:username] == USERNAME && params[:passwd] == PASSWORD
+    result[:success] = true
+  else
+    result[:errors] = []
+    result[:errors] << :username unless params[:username] == USERNAME
+    result[:errors] << :passwd unless params[:passwd] == PASSWORD
+  end
+
+  content_type :json
+  result.to_json
+end
